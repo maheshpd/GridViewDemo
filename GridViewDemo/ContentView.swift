77b05b5ab5a1f2b0fab9a401f3f5fd5689505f52
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-04-18 11:51:45 +0530

    Initial Commit

---
+++ GridViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridViewDemo
+//
+//  Created by Mahesh Prasad on 18/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridViewDemo/GridViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewDemoApp.swift
+//  GridViewDemo
+//
+//  Created by Mahesh Prasad on 18/04/21.
+//
+
+import SwiftUI
+
+@main
+struct GridViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
